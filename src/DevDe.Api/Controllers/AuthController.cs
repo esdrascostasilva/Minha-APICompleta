c6afe0db6188fbf81134d8d12e0b6be65f530c77
Esdras Costa <esdras.cts@outlook.com>
2021-09-17 22:04:47 -0300

    Finalized the authorization with JWT.

--- src/DevDe.Api/Controllers/AuthController.cs
+++ src/DevDe.Api/Controllers/AuthController.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -77,7 +78,7 @@ namespace DevDe.Api.Controllers
 
         }
 
-        private async Task<string> GerarJwt(string email)
+        private async Task<LoginRespondeViewModel> GerarJwt(string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
             var claims = await _userManager.GetClaimsAsync(user);
@@ -110,7 +111,19 @@ namespace DevDe.Api.Controllers
 
             var encodedToken = tokenHandler.WriteToken(token);
 
-            return encodedToken;
+            var response = new LoginRespondeViewModel
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UserToken = new UserTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c=> new ClaimViewModel { Type = c.Type, Value = c.Value })
+                }
+            };
+
+            return response;
         }
 
         private static long ToUnixEpochDate(DateTime date)
